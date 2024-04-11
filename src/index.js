2a21e6102a1463daf3043ffdbb9b0d6cd916d461
G O V I N D   U P A D H Y A Y <govind.upadhayay19@gmail.com>
2024-04-11 14:30:54 +0530

    connecting to db

---
+++ src/index.js
@@ -0,0 +1,48 @@
+// require('dotenv').config({path : './env'})
+import dotenv from "dotenv"
+import connectDB from "./db"
+
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* --> Establishing connection using effy method 
+;(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error" , (error) => {
+            console.log("ERRR: " , error)
+            throw error
+        })
+
+        app.listen(process.env.PORT , () => {
+            console.log(`Server Started at ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log("Error: " , error);
+        throw error
+    }
+})()
+
+*/
