c9063f6eb707d51b509f9a3a70386cb1eb5ad162
G O V I N D   U P A D H Y A Y <govind.upadhayay19@gmail.com>
2024-04-11 15:58:57 +0530

    Utils code discussion

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,12 @@
-const asyncHandler = () => {
-    
+const asyncHandler = (fn) = async (req , res , next) => {
+    try {
+        await fn(req , res , next);
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success:false,
+            message: error.message
+        })
+    }
 }
 
 export {asyncHandler};
