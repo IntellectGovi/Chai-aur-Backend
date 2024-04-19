03d917bad1e7cddb4ba5f76660ef59c5ae1bf182
G O V I N D   U P A D H Y A Y <govind.upadhayay19@gmail.com>
2024-04-19 12:24:01 +0530

    Error Resolved

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -101,7 +101,7 @@ const registerUser = asyncHandler( async (req, res) => {
     //console.log(req.files);
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
