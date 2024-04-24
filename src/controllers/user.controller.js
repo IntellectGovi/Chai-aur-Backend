c4d73eb54d6b6dc1c38b19b8cfaf03bfb50e1356
G O V I N D   U P A D H Y A Y <govind.upadhayay19@gmail.com>
2024-04-24 19:05:28 +0530

    Changes in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -267,7 +267,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const user = User.findByIdAndUpdate(req.user?._id, {
+  const user = await User.findByIdAndUpdate(req.user?._id, {
     $set: {
         fullName,
         email: email,
