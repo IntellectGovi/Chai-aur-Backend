0bd55ea23eb6ffddd42036a6daeb1ac43abd708f
G O V I N D   U P A D H Y A Y <govind.upadhayay19@gmail.com>
2024-04-25 19:33:52 +0530

    Added user,comment,playlst,like,tweet models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -168,8 +168,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     {
